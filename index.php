8c17d043fc477ab4be9472c003e71e91ee62e050
PASCAWINDRA <87888871+PASCAWINDRA@users.noreply.github.com>
2025-01-19 23:47:30 +0700

    ganti trending now di index.php jadi berita terbaru

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ include 'main.php';
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="trending-tittle">
-                                <strong>Trending now</strong>
+                                <strong>Berita Terbaru</strong>
                                 <!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
                                 <div class="trending-animated">
                                     <ul id="js-news" class="js-hidden">
